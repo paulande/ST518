452c46b73f7ce86b0ac29eb614859ecc236df997
paulande <andepaul@oregonstate.edu>
2025-03-30 15:52:29 -0700

    adding file structure

---
+++ Module_00/ST518.R
@@ -0,0 +1,5 @@
+# Oregon State University
+# CS513 Applied Machine Learning
+# Paul J Anderson
+# Project:
+# Date: 
---
+++ Module_00/module1.py
@@ -0,0 +1,22 @@
+# terminal setup for directories and sub folders
+
+cd C:\Users\paulande\source\repos
+mkdir CS513_ML
+cd CS513_ML
+mkdir Module_0 Module_1 Module_2 Module_3 Module_4 Module_5 Module_6 Module_7 Module_8 Module_9
+
+cd Module_0
+mkdir data notebooks scripts
+cd ..
+
+cd Module_1
+mkdir data notebooks scripts
+cd ..
+
+cd Module_2
+mkdir data notebooks scripts
+cd ..
+
+# Repeat for remaining modules
+
+git init
